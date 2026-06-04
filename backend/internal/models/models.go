132cc99d4c5fc20e491b65e135b59c426cfc0bec
fcharles <f.18charles@gmail.com>
2026-06-04 15:29:41 +0300

    feat(user): create getuserbyid function

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -7,20 +7,18 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `db:"id" json:"id"`
-	Email     string    `db:"email" json:"email"`
-	PassHash  string    `db:"password_hash" json:"pass_hash"`
-	FirstName string    `db:"first_name" json:"first_name"`
-	LastName  string    `db:"last_name" json:"last_name"`
-	DOB       time.Time `db:"dob" json: dob`
-	avatar    string    `db:"avatar" json:"avatar"`
-	Nickname  string    `db:"nickname" json:"nickname"`
-	AboutMe   string    `db:"about_me" json:"about_me"`
-
-	FollowerCount  int `db:"follower_count" json:"follower_count"`
-	FollowingCount int `db:"following_count" json:"following_count"`
-
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	ID             uuid.UUID `db:"id" json:"id"`
+	Email          string    `db:"email" json:"email"`
+	PassHash       string    `db:"password_hash" json:"pass_hash"`
+	FirstName      string    `db:"first_name" json:"first_name"`
+	LastName       string    `db:"last_name" json:"last_name"`
+	DOB            time.Time `db:"dob" json: dob`
+	avatar         string    `db:"avatar" json:"avatar"`
+	Nickname       string    `db:"nickname" json:"nickname"`
+	AboutMe        string    `db:"about_me" json:"about_me"`
+	FollowerCount  int       `db:"follower_count" json:"follower_count"`
+	FollowingCount int       `db:"following_count" json:"following_count"`
+	CreatedAt      time.Time `db:"created_at" json:"created_at"`
 }
 
 type Session struct {
