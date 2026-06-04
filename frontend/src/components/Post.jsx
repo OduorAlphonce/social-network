ee6aa8cbf933de38cd76840b5e57fa96602cf88b
Silas <writer.silas@gmail.com>
2026-06-04 16:07:57 +0300

    feat: add toggle classlist to reactions

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import "../styles/post.css";
 import { Dislike, Like } from "./Reactions";
 
-const Post = ({ like, dislike }) => {
-    
+const Post = () => {
+  const [likePost, setLikePost] = useState(false);
+  const [dislikePost, setDislikePost] = useState(false);
+
+  const dislikeReaction = document.getElementById("dislike");
+  const likeReaction = document.getElementById("like");
+
+  function like() {
+    if (dislikePost) {
+      setDislikePost(false);
+    }
+    setLikePost((prev) => !prev);
+    if (likePost) {
+      likeReaction.classList.add("reaction-active");
+      dislikeReaction.classList.remove("reaction-active");
+    } else {
+      likeReaction.classList.remove("reaction-active");
+    }
+  }
+
+  function dislike() {
+    if (likePost) {
+      setLikePost(false);
+    }
+    setDislikePost((prev) => !prev);
+    if (dislikePost) {
+      dislikeReaction.classList.add("reaction-active");
+      likeReaction.classList.remove("reaction-active");
+    } else {
+      dislikeReaction.classList.remove("reaction-active");
+    }
+  }
+
   return (
     <div className="post-container">
       <div className="post-header">
@@ -15,7 +47,7 @@ const Post = ({ like, dislike }) => {
       </div>
       <div className="post-footer">
         <Like like={like} />
-        <Dislike dislike={dislike}/>
+        <Dislike dislike={dislike} />
       </div>
     </div>
   );
--- frontend/src/components/Reactions.jsx
+++ frontend/src/components/Reactions.jsx
@@ -1,11 +1,11 @@
 import { BiDislike, BiLike } from "react-icons/bi";
 
 const Like = ({ like }) => {
-  return <BiLike onClick={like} size={24} />;
+  return <BiLike id = "like" onClick={like} size={24} />;
 };
 
 const Dislike = ({ dislike }) => {
-  return <BiDislike onClick={dislike} size={24}/>;
+  return <BiDislike id = "dislike" onClick={dislike} size={24}/>;
 };
 
 export { Like, Dislike };
