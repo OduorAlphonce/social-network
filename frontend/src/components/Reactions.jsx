e3946e56e3001d6fd8ed8e672c65c6c00e23d7d8
Silas <writer.silas@gmail.com>
2026-06-04 14:48:09 +0300

    feat: add like and dislike reaction buttons

---
+++ frontend/src/components/Reactions.jsx
@@ -0,0 +1,11 @@
+import { BiDislike, BiLike } from "react-icons/bi";
+
+const Like = () => {
+  return <BiLike />;
+};
+
+const Dislike = () => {
+  return <BiDislike />;
+};
+
+export { Like, Dislike };
