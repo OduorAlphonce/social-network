cc62c2dd37cecff30af8e74e0079241e0568d568
Silas <writer.silas@gmail.com>
2026-06-07 14:14:31 +0300

    feat: add layout template

---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import "../../styles/layout.css"
+
+const Layout = () => {
+  return (
+    <div className="main-container">
+      <aside className="sidebar">Sidebar</aside>
+      <div className="header">Header</div>
+      <div className="main-content">Body</div>
+    </div>
+  );
+};
+
+export default Layout;
