654e69aaeb186521469c3c9f61d8424e9bc8b4ca
Silas <writer.silas@gmail.com>
2026-06-07 15:30:38 +0300

    feat: add navigation icons and search on header as well as style header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import { IoArrowRedo } from "react-icons/io5";
+import "../styles/header.css";
+import { FaPlusCircle } from "react-icons/fa";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdOutlineGroup } from "react-icons/md";
+import avatar from "../assets/user.svg";
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="pretitle">
+        <IoArrowRedo />
+        <strong>CoreConnect</strong>
+        <input
+          type="text"
+          className="search"
+          placeholder="Search CoreConnect..."
+        />
+      </div>
+      <div className="icons">
+        <FaPlusCircle size={24} />
+        <AiOutlineMessage size={24} />
+        <IoIosNotificationsOutline size={24} />
+        <MdOutlineGroup size={24} />
+        <img src={avatar} className="profile-photo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
