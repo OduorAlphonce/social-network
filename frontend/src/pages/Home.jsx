a0a13c5053b7a284125c6cafbb1c057f3a9ec4c0
Silas <writer.silas@gmail.com>
2026-06-13 12:07:29 +0300

    feat: add quick links content divs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,12 +7,22 @@ import "../styles/home.css";
 function Home() {
   return (
     <div className="home-container">
-    <div className="posts">
-      {posts.map((it, idx) => {
-        return <Post key={idx} post={it} />;
-      })}
-    </div>
-    <div className="quick-links">Hello</div>
+      <div className="posts">
+        {posts.map((it, idx) => {
+          return <Post key={idx} post={it} />;
+        })}
+      </div>
+      <div className="quick-links">
+        <div>
+          <strong>Sponsored</strong>
+        </div>
+        <div>
+          <strong>Upcoming Events</strong>
+        </div>
+        <div>
+          <strong>Contacts</strong>
+        </div>
+      </div>
     </div>
   );
 }
