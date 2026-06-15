5983fcdb04c0d7b6e8f6ec76d3e9c3232cd4be2a
akihara <carsoniskihara@gmail.com>
2026-06-15 07:23:12 +0300

    chore(front-end): commented out unexisting imports and updated package-lock

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { posts } from "../assets/posts-data.js";
-import { comments } from "../assets/comments-data.js";
+// import { posts } from "../assets/posts-data.js";
+// import { comments } from "../assets/comments-data.js";
 import Post from "../components/Post.jsx";
 
 function Home() {
+  const posts = new Array()
+
   return (
     <div className="posts">
       {posts.map((it) => {
