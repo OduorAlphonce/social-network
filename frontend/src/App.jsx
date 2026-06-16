2f3c10915ce858603c6193f54c0ccae338b21d08
aloduor <alphonceoyunga@outlook.com>
2026-06-16 11:17:32 +0300

    feat(auth); add login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,13 @@ import Profile from "./pages/Profile.jsx";
 import Messages from "./pages/Messages.jsx";
 import Notifications from "./pages/Notifications.jsx";
 import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/profile" element={<Profile />} />
