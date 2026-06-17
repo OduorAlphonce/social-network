a1a85654412b93fb94ee89a85ac4f0c0b21fbe88
Silas <writer.silas@gmail.com>
2026-06-17 15:31:13 +0300

    fix: format comment

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,18 +1,22 @@
 import "../styles/comment.css";
 
 const Comment = ({ comment }) => {
+
+ 
   return (
-    <div classname="comment ">
+    <div id="comment-container">
       <img
         src={comment?.author?.avatar ? comment.author.avatar : avatar}
         alt="avatar"
         className="profile-photo"
       />
-      <div>
-        <strong>{comment?.name}</strong>
-        <p>{comment?.content}</p>
+      <div className="comment-body">
+        <div className="comment-details">
+          <strong>{comment?.name}</strong>
+          <p>{comment?.content}</p>
+        </div>
+        <div>{comment?.time} <span>Like</span>  <span>Reply</span></div>
       </div>
-      <span>{comment.time}</span> <span>like</span> <span>reply</span>
     </div>
   );
 };
