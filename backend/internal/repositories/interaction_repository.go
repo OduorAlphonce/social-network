fc993ab0aaf63353ccedf1630d6c7f7d3dbfdb95
Ashomondi <adhiamboa745@gmail.com>
2026-06-17 15:08:19 +0300

    Create InteractionRepository struct to manage user post-engagement data

---
+++ backend/internal/repositories/interaction_repository.go
@@ -0,0 +1,18 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+)
+
+type InteractionRepository struct {
+	db *sql.DB
+}
+
+func NewInteractionRepository (db *sql.DB) *InteractionRepository {
+	return &InteractionRepository{db: db}
+}
+
+func (r *InteractionRepository) GetViewerVote(ctx context.Context, viewerID string, postID string) (*string, error) {
+	return nil, nil
+}
