e64a404bf92ede91a92374c5f5bdaa41fb4b398f
qquinton <qquinton@example.com>
2026-06-20 17:11:30 +0300

    feature: add rollback function

--- backend/internal/api/routers/router.go
+++ backend/internal/api/routers/router.go
@@ -64,7 +64,13 @@ func RegisterRoutes(
 	mux.Handle("/api/users/{id}/posts", authMiddleware(http.HandlerFunc(postHandler.ProfilePosts)))
 
 	mux.Handle("/api/posts/{id}", authMiddleware(http.HandlerFunc(postHandler.GetSinglePost)))
-	mux.Handle("/api/posts/{id}/comments", authMiddleware(http.HandlerFunc(postHandler.GetComments)))
+	mux.Handle("/api/posts/{id}/comments", authMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			postHandler.CreateComment(w, r)
+		} else {
+			postHandler.GetComments(w, r)
+		}
+	})))
 
 	// Groups routes
 	mux.Handle("/api/groups", authMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
