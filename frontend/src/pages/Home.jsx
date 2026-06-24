e17f342130b4547a906ae03c104f223601c29629
Silas <writer.silas@gmail.com>
2026-06-24 13:59:42 +0300

    feat: add new post page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,16 @@ import Post from "../components/Post.jsx";
 import "../styles/home.css";
 
 function Home() {
-  const Allposts = posts;
+  const [Allposts, setAllposts] = useState(initialPosts);
+
+  function handleNewPost(created) {
+    setAllposts((prev) => [created, ...prev]);
+  }
 
   return (
     <div className="home-container">
       <div className="posts">
+        <NewPost onCreate={handleNewPost} />
         {Allposts?.map((it, idx) => {
           return <Post key={idx} post={it} />;
         })}
